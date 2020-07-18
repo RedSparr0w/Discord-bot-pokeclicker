49483cce0f02dc74dd8ef0c985bf6886796890cf
RedSparr0w <admin@redsparr0w.com>
2020-07-19 11:25:13 +1200

    Update shards display

--- commands/shards.js
+++ commands/shards.js
@@ -5,15 +5,17 @@ module.exports = {
   name        : 'shards',
   aliases     : ['s', 'shard'],
   description : 'Get a list of routes where you can obtain a particular type of shard',
-  args        : ['type'],
+  args        : ['type', 'order(chance|route)?'],
   guildOnly   : true,
   cooldown    : 3,
   botperms    : ['SEND_MESSAGES'],
   userperms   : ['SEND_MESSAGES'],
   execute     : async (msg, args) => {
-    let [type] = args;
+    let [type, order] = args;
     type = type.charAt(0).toUpperCase() + type.slice(1).toLowerCase();
     if (!(PokemonType[type] >= 0)) return msg.reply(`Invalid type: \`${type}\``);
+    
+    const sortFunc = order == 'chance' ? (a, b) => b[1] - a[1] : (a, b) => a[0] - b[0];
 
     const embed = new MessageEmbed()
       .setTitle(`${pokemonTypeIcons[type]} ${type} Shard Routes`)
@@ -25,9 +27,10 @@ module.exports = {
     Object.entries(shardRoutes).forEach(([region, routes]) => {
       if (!Object.entries(routes).length) return;
       const bestShardRoute = findShardBestRoute(RouteShardTypes, PokemonType[type], region);
-      const description = ['Best Route:', `[${bestShardRoute.route}] ${bestShardRoute.chance}%`, '\nAll Routes:'];
-      Object.entries(routes).forEach(([route, chance]) => {
-        description.push(`[${route}] ${+chance.toFixed(2)}%`);
+      const description = ['Best Route:', `${`[${bestShardRoute.route}]`.padEnd(4, ' ')} ${bestShardRoute.chance.toFixed(1).padStart(4,' ')}%`, '\nAll Routes:'];
+      
+      Object.entries(routes).sort(sortFunc).forEach(([route, chance]) => {
+        description.push(`${`[${route}]`.padEnd(4, ' ')} ${chance.toFixed(1).padStart(4,' ')}%`);
       });
       embed.addField(`❯ ${GameConstants.Region[region].toUpperCase()}`, `\`\`\`ini\n${description.join('\n')}\n\`\`\``, true);
     });
