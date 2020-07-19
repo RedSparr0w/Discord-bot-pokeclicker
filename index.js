f5706b52f396352a86d2c7f399058fc2667aaeb9
RedSparr0w <admin@redsparr0w.com>
2020-07-20 01:02:16 +1200

    Add reactions

--- index.js
+++ index.js
@@ -31,8 +31,27 @@ client.once('ready', async() => {
 client.on('error', e => error('Client error thrown:', e))
   .on('warn', warning => warn(warning))
   .on('message', message => {
-    // Either not a command or a message from a bot, we can ignore this
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    // Either not a command or a bot, ignore
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) {
+      // Auto react to comments if 2+ lines start with an emoji
+      const emoji_regex = /^((?:<:.+?:)\d+|[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/;
+      const lines = message.content.split(/\r?\n/).map(line=>line.trim());
+      const answers = lines.filter(line=>emoji_regex.test(line));
+      if (answers.length < 2) return;
+      const addReaction = (reaction, cb) =>{
+        setTimeout(async () => {
+          await message.react(reaction).catch(O_o=>{});
+          cb();
+        }, 100);
+      };
+
+      const reactions = answers.map(answer=>answer.match(emoji_regex)[1]);
+      reactions.reduce((promiseChain, reaction) => promiseChain.then(() => new Promise((resolve) => {
+        addReaction(reaction, resolve);
+      })), Promise.resolve());
+      return;
+    }
 
     // Each argument should be split by 1 (or more) space character
     const args = message.content.slice(prefix.length).trim().split(/,?\s+/);
