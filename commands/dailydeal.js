a9be81ff73a89a90dfe57f7cee6bbccacc3685ec
RedSparr0w <admin@redsparr0w.com>
2020-07-19 13:42:24 +1200

    Show profit for daily deals

--- commands/dailydeal.js
+++ commands/dailydeal.js
@@ -26,14 +26,31 @@ module.exports = {
 
     const today = new Date();
     const dateToCheck = new Date(today.getUTCFullYear(), today.getUTCMonth(), today.getUTCDate());
+
+    const calculateProfit = (deal) => {
+      if (deal.item1.value >= 100 || deal.item1.value <= 1) deal.item1.value = 0;
+      if (deal.item2.value >= 100 || deal.item2.value <= 1) deal.item2.value = 0;
+      return (deal.item2.value * deal.amount2) - (deal.item1.value * deal.amount1);
+    };
+
+    const calculateProfitString = (deal) => {
+      const profit = calculateProfit(deal);
+      return profit == 0 ? '---' : profit > 0 ? `+${profit.toString().padEnd(2, ' ')}💎` : `${profit.toString().padEnd(3, ' ')}💎`;
+    };
+
     for (let i = 0; i < 5; i++) {
       DailyDeal.generateDeals(5, dateToCheck);
       const description = ['```prolog'];
+      const profit = ['```diff'];
       DailyDeal.list.forEach(deal => {
         description.push(`${deal.amount1.toString().padStart(2, ' ')} × ${deal.item1.name.toString().padEnd(padding, ' ')}  →  ${deal.amount2.toString().padStart(2, ' ')} × ${deal.item2.name}`);
+        profit.push(calculateProfitString(deal));
       });
       description.push('```');
-      embed.addField(`❯ ${dateToCheck.getFullYear()}-${(dateToCheck.getMonth() + 1).toString().padStart(2, 0)}-${dateToCheck.getDate().toString().padStart(2, 0)}`, description.join('\n'));
+      profit.push('```');
+      embed.addField('Profit', profit.join('\n'), true);
+      embed.addField(`❯ ${dateToCheck.getFullYear()}-${(dateToCheck.getMonth() + 1).toString().padStart(2, 0)}-${dateToCheck.getDate().toString().padStart(2, 0)}`, description.join('\n'), true);
+      embed.addField('\u200b', '\u200b', true); // To take up the third row
       dateToCheck.setDate(dateToCheck.getDate() + 1);
     }
 
