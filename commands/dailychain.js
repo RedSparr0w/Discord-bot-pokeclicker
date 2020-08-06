5bfe94c5d8c11826a90d077d19ca371bfb7c39dd
Jessica Scoltock <jessica.scoltock@protonmail.com>
2020-08-06 22:04:33 +0000

    replace chain finding algorithm with a better time complexity (#3)
    * replace chain finding algorithm with a better time complexity
    
    * Fix profit toLocaleString
    
    * Limit length of field output to 1000 per field (Discord Limit)
    
    * Split into columns to allow longer chains
    
    * Limit chain lengths to Discord limits
    
    Co-authored-by: RedSparr0w <admin@redsparr0w.com>

--- commands/dailychain.js
+++ commands/dailychain.js
@@ -37,7 +37,9 @@ module.exports = {
     if (isNaN(days) || days <= 0) {
       days = 14;
     } else {
-      days = Math.min(150, days);
+      // speed is no longer an issue,
+      // but we are limited by message length limits
+      days = Math.min(1000, days);
     }
 
     const embed = new MessageEmbed()
@@ -49,32 +51,6 @@ module.exports = {
     // Calculate name padding
     const allItemsLength = UndergroundItem.list.map(item => item.name.length);
     const padding = Math.max(...allItemsLength);
-
-    const itemsByValue = [...UndergroundItem.list].sort((a, b) => {
-      const valueA = (a.value >= 100 || a.value <= 1) ? 0 : a.value;
-      const valueB = (b.value >= 100 || b.value <= 1) ? 0 : b.value;
-      return valueB - valueA;
-    });
-    const diamondItemsByValue = itemsByValue.filter(item => item.valueType == 'Diamond');
-    const diamondItemNamesByValue = diamondItemsByValue.map(item => item.name);
-
-    const calcProfit = (dealOne, dealTwo) => dealOne.amount2 * dealTwo.amount2 / dealOne.amount1 / dealTwo.amount1;
-
-    const calcChainProfit = (deals) => {
-      const first = deals[0];
-      let second = deals[1];
-      const buyin = first.item1.value;
-      if (!second) {
-        return (first.item2.value * first.amount2 / first.amount1) - buyin;
-      }
-      let amt = calcProfit(first, second);
-      let i = 1;
-      while (deals[++i]) {
-        second = deals[i];
-        amt *= second.amount2 / second.amount1;
-      }
-      return (amt * second.item2.value) - buyin;
-    };
 
     class DealProfit {
       constructor(type, amount) {
@@ -111,29 +87,42 @@ module.exports = {
       return dealsList;
     };
 
-    const dailyDeals = getDealsList(fromDate, days, maxSlots);
+    const dailyDeals = getDealsList(fromDate, days, maxSlots).flatMap(deals =>
+      // when processing deals, we want to make sure all possible links
+      // have been processed. Sometimes, there is a deal on the same day
+      // which we can link to, so we sort the deals within each day to
+      // make sure those linkables have been processed
+      deals.sort((a, b) => {
+        // if `b` can link to `a`, `a` should sort after `b` (and vice versa)
+        if (a.item1 == b.item2) return 1;
+        if (b.item1 == a.item2) return -1;
 
-    const possibleTradesGive = {};
-    const possibleTradesGet = {};
-    [...dailyDeals].reverse().forEach(deals => {
-      deals.forEach(deal => {
-        if (!possibleTradesGive[deal.item1.name]) possibleTradesGive[deal.item1.name] = [];
-        possibleTradesGive[deal.item1.name].push(deal);
-        if (!possibleTradesGet[deal.item2.name]) possibleTradesGet[deal.item2.name] = [];
-        possibleTradesGet[deal.item2.name].push(deal);
-      });
-    });
+        // if `a` can be linked from something sort `a` after `b`
+        if (deals.find(x => a.item1 == x.item2)) return 1;
+        // if `b` can be linked from something, sort `a` before `b`
+        if (deals.find(x => b.item1 == x.item2)) return -1;
+
+        return 0;
+      })
+    );
 
     const chainList = [];
     let worstProfitInList = 0;
     const maxChains = 20;
 
-    const addChain = (dealList) => {
-      const profit = calcChainProfit(dealList);
+    const addChain = (start) => {
+      const profit = start.profit - start.deal.item1.value;
       if (profit > worstProfitInList) {
-        chainList.push({profit: profit, deals: dealList});
-        chainList.sort((a,b) => (b.profit - a.profit));
+        const chain = [];
+        let link = start;
 
+        while (link != undefined) {
+          chain.push(link.deal);
+          link = chains.list[link.next];
+        }
+
+        chainList.push({profit: profit, deals: chain});
+        chainList.sort((a,b) => (b.profit - a.profit));
         if (chainList.length > maxChains) {
           chainList.pop();
           worstProfitInList = chainList[chainList.length - 1].profit;
@@ -141,58 +130,92 @@ module.exports = {
       }
     };
 
-    const createDealsChain = (itemName, date, currentChain = [], chainValue = 1) => {
-      if (possibleTradesGet[itemName]) {
-        const nextDeals = possibleTradesGet[itemName].filter(nDeal => nDeal.date <= date);
-        if (nextDeals.length) {
-          nextDeals.forEach(deal => {
-            // If we would be better off selling item1 of this deal,
-            // then we shouldn't suggest feeding it into the chain
-            const newChainValue = chainValue * deal.amount2 / deal.amount1;
-            const potentialProfit = newChainValue - (deal.amount1 * deal.item1.value);
+    const betterToSell = (deal, next) => {
+      // If we would be better off selling item1 of this deal,
+      // then we shouldn't suggest feeding it into the chain
+      const newChainValue = next.profit * deal.amount2 / deal.amount1;
+      const potentialProfit = newChainValue - deal.item1.value;
 
-            // If we wouldn't get diamonds from selling item1, we only stand to gain
-            const notDia = deal.amount1 >= 100 || deal.amount1 < 0;
+      // If we wouldn't get diamonds from selling item1, we only stand to gain
+      const notDia = deal.amount1 >= 100 || deal.amount1 < 0;
 
-            if (notDia || potentialProfit > 0) {
-              createDealsChain(deal.item1.name, deal.date, [deal, ...currentChain], newChainValue);
-            }
-          });
-        } else {
-          addChain(currentChain);
-        }
-      } else {
-        addChain(currentChain);
-      }
+      return (!notDia && potentialProfit <= 0);
     };
 
-    diamondItemNamesByValue.forEach(item => {
-      if (!possibleTradesGet[item]) return;
-      possibleTradesGet[item].forEach(deal => {
-        if (deal.profit.amount > 0) {
-          const profitMult = deal.item2.value * deal.amount2 / deal.amount1;
-          createDealsChain(deal.item1.name, deal.date, [deal], profitMult);
-        }
-      });
-    });
+    const chains = dailyDeals.reduceRight((res,deal) => {
+      // Link to the best future deal
+      let next = res.bestStartingWith[deal.item2.name];
+
+      // Don't link to a bad chain
+      if (next != undefined && betterToSell(deal, next)) {
+        next = undefined;
+      }
+
+      const chainlink = {deal: deal, next: next, linkedFrom: []};
+      const index = res.list.length;
+
+      // Calculate chain value from this deal
+      if (next != undefined) {
+        const nextlink = res.list[next];
+        nextlink.linkedFrom.push(index);
+        chainlink.profit = nextlink.profit * deal.amount2 / deal.amount1;
+      } else {
+        const val = (deal.item2.value > 100 || deal.item2.value < 0) ? 0 : deal.item2.value;
+        chainlink.profit = val * deal.amount2 / deal.amount1;
+      }
+
+      // update bestStartingWith
+      const bestItem1 = res.list[res.bestStartingWith[deal.item1.name]];
+      if (chainlink.profit > 0 && (!bestItem1 || bestItem1.profit < chainlink.profit)) {
+        res.bestStartingWith[deal.item1.name] = index;
+      }
+
+      res.list.push(chainlink);
+      return res;
+    }, { bestStartingWith: {}, list: [] });
+
+    // build chainList
+    // only add chains using the start, ie those that aren't linkedFrom anything
+    chains.list.forEach(link => link.linkedFrom.length || addChain(link));
 
     let tooLong = false;
-    chainList.filter(i => i.deals.length > 0).forEach((chain, index) => {
+    chainList.forEach(chain => {
       if (tooLong) return;
-      let description = [];
+      // Our data
       const deals = chain.deals;
-      description.push(`Profit per 1 of initial investment \`💎 ${+chain.profit.toFixed(1)}\``);
-      description.push('```ini');
-      deals.forEach((deal, i) => {
-        description.push(`[${dateToString(deal.date)}] [${deal.amount1}] ${deal.item1.name.padEnd(padding, ' ')} → [${deal.amount2}] ${deal.item2.name}`);
-      });
-      description.push('```');
+      const profit = +chain.profit.toFixed(1);
+      // Title
       const title = `❯ ${dateToString(deals[0].date)} → ${dateToString(deals[deals.length - 1].date)}`;
-      description = description.join('\n');
+      const description = `Profit per 1 of initial investment \`💎 ${profit.toLocaleString('en-US')}\``;
       if (embed.length + title.length + description.length >= 6000) {
         return tooLong = true;
       }
       embed.addField(title, description);
+
+      // Deals Output
+      const dates = [];
+      const deal_output1 = [];
+      const deal_output2 = [];
+      deals.forEach(deal => {
+        dates.push(`[${dateToString(deal.date)}]`);
+        deal_output1.push(`[${deal.amount1}] ${deal.item1.name.padEnd(padding, ' ')}`);
+        deal_output2.push(`[${deal.amount2}] ${deal.item2.name}`);
+      });
+
+      while (deals.length) {
+        const max_size = Math.floor(1024 / (`${deal_output1[2]}\n`).length) - 1;
+        deals.splice(0, max_size);
+        const date_str = ['```ini', ...dates.splice(0, max_size), '```'].join('\n');
+        const deal_1_str = ['```ini', ...deal_output1.splice(0, max_size), '```'].join('\n');
+        const deal_2_str = ['```ini', ...deal_output2.splice(0, max_size), '```'].join('\n');
+        if (embed.length + date_str.length + deal_1_str.length + deal_2_str.length + 50 /* account for title + too long message lengths */ >= 6000) {
+          embed.addField('...chain length too long...', '\u200b', false);
+          return tooLong = true;
+        }
+        embed.addField('Date', date_str, true);
+        embed.addField('Give', deal_1_str, true);
+        embed.addField('Receive', deal_2_str, true);
+      }
     });
 
 
