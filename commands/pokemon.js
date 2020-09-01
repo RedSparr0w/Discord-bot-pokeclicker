561f87f42c2490ea2ee381d6c849479c0857207d
RedSparr0w <admin@redsparr0w.com>
2020-09-01 12:17:29 +1200

    Only add the extra spacing if Pokemon is obtainable

--- commands/pokemon.js
+++ commands/pokemon.js
@@ -60,9 +60,9 @@ module.exports = {
       .addField('<:RareCandySmall:733974449774133299> Level Type', `${LevelType[pokemon.levelType]}`, true)
       .addField('\u200b', '\u200b', true) // Spacing
       .addField('<:Pokemon_egg:733973219177922591> Egg Steps', `${pokemon.eggSteps}`, true)
-      .addField('\u200b', '\u200b'); // Spacing
 
     if (pokemon.locations && Object.keys(pokemon.locations).length) {
+      embed.addField('\u200b', '\u200b'); // Spacing
       // Routes
       if (pokemon.locations[PokemonLocationType.Route]) {
         let description = '';
