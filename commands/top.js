55b72e5497798262231839087b7bd1aabb7ae811
RedSparr0w <admin@redsparr0w.com>
2020-09-07 13:32:41 +1200

    Update top command

--- commands/top.js
+++ commands/top.js
@@ -1,26 +1,29 @@
+const { MessageEmbed } = require('discord.js');
 const { getTop } = require('../database.js');
 
 module.exports = {
   name        : 'top',
-  aliases     : ['leaderboard'],
+  aliases     : ['leaderboard', 'lb'],
   description : 'Get a list of users with the most points',
   args        : ['amount(10)?'],
   guildOnly   : true,
   cooldown    : 3,
   botperms    : ['SEND_MESSAGES'],
-  userperms   : ['MANAGE_MESSAGES'],
+  userperms   : ['SEND_MESSAGES'],
   execute     : async (msg, args) => {
     const amount = +(args.find(arg=>/^\d+$/.test(arg)) || 10);
 
     // Check user has entered a valid amount
     if (isNaN(amount) || amount < 1 || amount > 40) return msg.channel.send('Invalid amount specified, Must be between 1 and 40...');
 
-    const results = await getTop(msg.guild, amount);
+    const results = await getTop(amount);
 
-    const output = [`__***Top ${results.length} users:***__`, ...results.map((res, place) => `**#${place + 1}** _\`(${res.points.toLocaleString('en-NZ')} points)\`_ ${msg.guild.members.cache.get(res.user) || 'Inactive Member'}`)];
-    if (output.join('\n').length >= 2000)
-      return msg.reply('Sorry this list is too large for discord, try a smaller amount');
+    const embed = new MessageEmbed()
+      .setTitle(`__***Top ${results.length} Trainers:***__`)
+      .setColor('#3498db');
+      
+    embed.addField('\u200b', results.map((res, place) => `**#${place + 1}** \`${res.amount ? res.amount.toLocaleString('en-NZ') : 0}\` <:money:737206931759824918> ${msg.guild.members.cache.get(res.user) || 'Inactive Member'}`));
 
-    msg.channel.send('Gathering Data...').then(m=>m.edit(output));
+    msg.channel.send({ embed });
   },
 };
