683a29e986e8ed1218d53d638dbf57f904e76756
RedSparr0w <admin@redsparr0w.com>
2020-12-12 21:45:18 +1300

    Add Happy Hour command

---
+++ commands/happyhour.js
@@ -0,0 +1,26 @@
+const { MessageEmbed } = require('discord.js');
+const {
+  HOUR,
+} = require('../helpers.js');
+
+module.exports = {
+  name        : 'happyhour',
+  aliases     : ['hh'],
+  description : 'Check when the next happy hour is',
+  args        : [],
+  guildOnly   : true,
+  cooldown    : 3,
+  botperms    : ['SEND_MESSAGES', 'EMBED_LINKS'],
+  userperms   : ['SEND_MESSAGES'],
+  channels    : ['bot-commands', 'game-corner', 'bot-coins'],
+  execute     : async (msg, args) => {
+    const now = Date.now();
+    const happy_hour = new Date((now - (now % (9 * HOUR))) + 9 * HOUR);
+    
+    const embed = new MessageEmbed()
+      .setDescription('Next happy hour:')
+      .setTimestamp(happy_hour)
+      .setColor('#3498db');
+    return msg.channel.send({ embed });
+  },
+};
