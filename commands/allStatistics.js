a74599201832e62eba71e3544d19c0c66d97c52f
RedSparr0w <admin@redsparr0w.com>
2021-02-24 00:41:51 +1300

    Make type optional

--- commands/allStatistics.js
+++ commands/allStatistics.js
@@ -5,7 +5,7 @@ module.exports = {
   name        : 'allstatistics',
   aliases     : ['allstats'],
   description : 'Get an overview of your statistics for this server',
-  args        : ['type'],
+  args        : ['type?'],
   guildOnly   : true,
   cooldown    : 3,
   botperms    : ['SEND_MESSAGES', 'EMBED_LINKS'],
@@ -16,30 +16,41 @@ module.exports = {
 
     const embed = new MessageEmbed().setColor('#e74c3c');
 
-    if (!type) {
-      embed.setDescription('No stat type mentioned..');
-      return msg.channel.send({ embed });
-    }
-
     const statTypes = await getStatisticTypes();
 
-    const stat = statTypes.find(s => s.name == type);
+    if (!type) {
+      const results = await Promise.all(statTypes.map(s => getOverallStatistic(s.name)));
 
-    if (!stat) {
-      embed.setTitle('Overall Statistics')
-        .setDescription(statTypes.map(s => s.name));
+      const padding = {
+        name: Math.max(4, ...results.map(r => r.name.length)),
+        users: Math.max(5, ...results.map(r => r.users.toLocaleString('en-US').length)),
+        value: Math.max(5, ...results.map(r => r.value.toLocaleString('en-US').length)),
+      };
+
+      embed.setTitle('__***Overall Statistics***__')
+        .setColor('#3498db')
+        .setDescription(['```js', `${'name'.padEnd(padding.name, ' ')} | ${'users'.padStart(padding.users, ' ')} | ${'value'.padStart(padding.value, ' ')}`, ''.padStart(6 + padding.name + padding.users + padding.value, '-'), ...results.map(r => `${r.name.padEnd(padding.name, ' ')} | ${r.users.toLocaleString('en-US').padStart(padding.users, ' ')} | ${r.value.toLocaleString('en-US').padStart(padding.value, ' ')}`), '```']);
+
+      return msg.channel.send({ embed });
+    } else {
+      const stat = statTypes.find(s => s.name == type);
+
+      if (!stat) {
+        embed.setTitle('Overall Statistics')
+          .setDescription(statTypes.map(s => s.name));
+        return msg.channel.send({ embed });
+      }
+
+      const stats = await getOverallStatistic(type);
+
+      embed.setTitle(`__***${stats.name}***__`)
+        .setColor('#3498db')
+        .setDescription([
+          `**❯ Users:** ${stats.users.toLocaleString('en-US')}`,
+          `**❯ Value:** ${stats.value.toLocaleString('en-US')}`,
+        ]);
+
       return msg.channel.send({ embed });
     }
-
-    const stats = await getOverallStatistic(type);
-
-    embed.setTitle(`__***${stats.name}***__`)
-      .setColor('#3498db')
-      .setDescription([
-        `**❯ Users:** ${stats.users.toLocaleString('en-US')}`,
-        `**❯ Value:** ${stats.value.toLocaleString('en-US')}`,
-      ]);
-
-    return msg.channel.send({ embed });
   },
 };
