1909e2941a6b1f0d194f67f2d1171f3fd5bb1324
Danial <admin@redsparr0w.com>
2021-04-28 18:29:19 +1200

    Add roleinfo command

---
+++ commands/roleinfo.js
@@ -0,0 +1,29 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+  name        : 'roleinfo',
+  aliases     : [],
+  description : 'Get servers role info',
+  args        : [],
+  guildOnly   : true,
+  cooldown    : 3,
+  botperms    : ['SEND_MESSAGES', 'EMBED_LINKS'],
+  userperms   : ['MANAGE_GUILD'],
+  channels    : [],
+  execute     : async (msg, args) => {
+    const guild = msg.guild;
+
+    const roles = guild.roles.cache;
+    const roleMemberCount = roles.sort((a, b) => b.rawPosition - a.rawPosition).map(r => {
+      const members = roles.get(r.id).members;
+      return `${r}: ${members.size}`;
+    }).join('\n');
+
+    const embed = new MessageEmbed()
+      .setColor('RANDOM')
+      .addField('Role Members:', roleMemberCount)
+      .setTimestamp();
+
+    return msg.channel.send({ embed: embed });
+  },
+};
