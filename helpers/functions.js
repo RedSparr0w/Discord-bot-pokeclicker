af460f636e52ec73f58b13bcbd7a3940347d8254
RedSparr0w <RedSparr0w@users.noreply.github.com>
2024-02-06 15:51:30 +1300

    lint

--- helpers/functions.js
+++ helpers/functions.js
@@ -137,7 +137,7 @@ const saveFileFlags = (saveData) => {
   }
   // If more than 100k Master balls, or more obtained than obtained
   const masterballsObtained = saveData.save?.statistics?.pokeballsObtained?.[GameConstants.Pokeball.Masterball] || 0;
-  const masterballsTotal = (saveData.save?.pokeballs?.pokeballs?.[GameConstants.Pokeball.Masterball] || 0) + (saveData.save?.statistics.pokeballsUsed?.[GameConstants.Pokeball.Masterball] || 0)
+  const masterballsTotal = (saveData.save?.pokeballs?.pokeballs?.[GameConstants.Pokeball.Masterball] || 0) + (saveData.save?.statistics.pokeballsUsed?.[GameConstants.Pokeball.Masterball] || 0);
   if (masterballsTotal > masterballsObtained || masterballsTotal > 1e5) {
     flags.push('Masterballs');
   }
