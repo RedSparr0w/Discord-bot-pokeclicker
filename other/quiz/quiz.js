c38ff3e136e27ce40091626f7cfb135aab0436d9
Jessica Chen <jessica.scoltock@protonmail.com>
2024-04-09 01:09:24 -0400

    There were no bugs in ba sing se

--- other/quiz/quiz.js
+++ other/quiz/quiz.js
@@ -115,6 +115,11 @@ const newQuiz = async (guild, reoccur = false) => {
 
   const incorrectFilter = quiz_channel.createMessageCollector({ filter: (m) => !filter(m), time: time_limit});
   incorrectFilter.on('collect', async m => {
+    // stop doing emoji when we're already done pls thx you
+    if (winners.has(user.id) || m.createdTimestamp - finished > ANSWER_TIME_LIMIT) {
+      return;
+    }
+
     const reaction = quiz.incorrectReaction?.(m.content);
 
     if (reaction) {
