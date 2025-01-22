a781b960cf7bb976c28ff7ac6506ccb86a4a83e7
RedzepR <143606926+RedzepR@users.noreply.github.com>
2025-01-22 19:23:10 +0100

    missing )

--- other/quiz/quiz_questions.js
+++ other/quiz/quiz_questions.js
@@ -158,7 +158,7 @@ const whatIsThatBerry = () => new Promise(resolve => {
 
 const howDoesThisPokemonEvolve = () => new Promise(resolve => {
   (async () => {
-      const pokemon = randomFromArray(pokemonListWithEvolution.filter(p => p.evolutions.some(e => e.trigger === 1 || e.trigger === 2));
+      const pokemon = randomFromArray(pokemonListWithEvolution.filter(p => p.evolutions.some(e => e.trigger === 1 || e.trigger === 2)));
         const allEligableEvolutions = pokemon.evolutions.filter(e => e.trigger === 1 || e.trigger === 2);
     const allEvolvedNames = [... new Set(allEligableEvolutions.map(e => e.evolvedPokemon))];
     const levelEvolution = [
