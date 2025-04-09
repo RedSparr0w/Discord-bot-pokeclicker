476e6154744f836a9a99708756a5bc7cb0db4f50
RedzepR <143606926+RedzepR@users.noreply.github.com>
2025-04-09 11:46:09 +0200

    Fix iki town outskirts bug following the toxtricity fix

--- other/quiz/quiz_questions.js
+++ other/quiz/quiz_questions.js
@@ -593,8 +593,8 @@ const pokemonFossil = () => {
 const dockTown = () => {
   const town = randomFromArray(GameConstants.DockTowns);
   const region = GameConstants.DockTowns.findIndex(t => t == town);
-  const answer = new RegExp(`^\\W*${town.replace(/\s*(town|city|island)/i, '').replace(/\W/g, '.?')}\\b`, 'i');
-  
+  const answer = new RegExp(`^\\W*${town.replace(/\W/g, '.?').replace(/(town|city|island)/i, '($1)?')}\\b`, 'i');
+
   const amount = getAmount();
 
   const description = [`Where abouts is the Dock located in the ${upperCaseFirstLetter(GameConstants.Region[region])} region?`];
@@ -619,8 +619,8 @@ const dockTown = () => {
 const startingTown = () => {
   const town = randomFromArray(GameConstants.StartingTowns);
   const region = GameConstants.StartingTowns.findIndex(t => t == town);
-  const answer = new RegExp(`^\\W*${town.replace(/\s*(town|city|island)/i, '').replace(/\W/g, '.?')}\\b`, 'i');
-  
+  const answer = new RegExp(`^\\W*${town.replace(/\W/g, '.?').replace(/(town|city|island)/i, '($1)?')}\\b`, 'i');
+
   const amount = getAmount();
 
   const description = [`Where does the player start in the ${upperCaseFirstLetter(GameConstants.Region[region])} region?`];
@@ -1112,7 +1112,7 @@ const quizTypes = [
   new WeightedOption(pokemonID, 60),
   new WeightedOption(fossilPokemon, 5),
   new WeightedOption(pokemonFossil, 5),
-  new WeightedOption(startingTown, 10),
+  new WeightedOption(startingTown, 10000000),
   new WeightedOption(dockTown, 10),
   new WeightedOption(whatIsThatBerry, 20),
   new WeightedOption(badgeGymLeader, 10),
