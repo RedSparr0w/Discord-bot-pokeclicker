befbb4f1257d1227b9224d8b1ffab7c8349a60a3
Martius RegisCoaxans <reiiky91300@gmail.com>
2025-04-27 21:23:21 +0200

    Forgot to clear icons...

--- slash_commands/mine-sweeper.js
+++ slash_commands/mine-sweeper.js
@@ -2,8 +2,8 @@ const { ApplicationCommandOptionType } = require('discord.js');
 const { serverIcons } = require('../config.js');
 
 const iconMap = {
-  '-2' : serverIcons.mineBombShiny ?? '<a:v:1365757752856678591>',
-  '-1' : serverIcons.mineBomb ?? '<a:v:1365757505841528853>',
+  '-2' : serverIcons.mineBombShiny ?? '💥',
+  '-1' : serverIcons.mineBomb ?? '🧨',
   '0' : '🟩',
   '1' : '1️⃣',
   '2' : '2️⃣',
