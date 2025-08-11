37a2be5e03437d9a4c682e9eb292e5df9101d836
Danial <admin@redsparr0w.com>
2025-08-11 20:35:26 +1200

    Add other languages, ignore names that are the same as the English name answer

--- other/quiz/quiz_questions.js
+++ other/quiz/quiz_questions.js
@@ -40,7 +40,7 @@ const defaultEndFunction = (title, image, description) => async (m, e) => {
   m.channel.send({ embeds: [embed] }).catch((...args) => warn('Unable to post quiz answer', ...args));
 };
 const getPokemonByName = name => pokemonList.find(p => p.name == name);
-const pokemonNameNormalized = (name) => translatePokemonName(name).replace(/\s?\([^|)]+\)/g, '').replace(/.*(Magikarp).*/, translatePokemonName('Magikarp')).replace(/(?!\|)\W/g, '.?').replace(/.*((Segin|Schedar|Segin|Ruchbah|Caph)\.\?Starmobile).*/, '($1)|(Revavroom)').replace(/(Valencian|Pinkan|Pink|Handout|Charity|Blessing|Crystal|Titan)\s*/gi, '($1)?').replace(/Noble\s*/g, '(Noble|Hisuian)?\\s*').replace('Toxtricity', 'Toxtri(city|town|island)?city').replace('Cosmog', 'Cosmog|Nebby');
+const pokemonNameNormalized = (name) => translatePokemonName(name).replace(/\s?\([^|)]+\)/g, '').replace(/.*(Magikarp).*/, translatePokemonName('Magikarp')).replace(/(\?!\|-_♂♀)/g, '.?').replace(/.*((Segin|Schedar|Segin|Ruchbah|Caph)\.\?Starmobile).*/, '($1)|(Revavroom)').replace(/(Valencian|Pinkan|Pink|Handout|Charity|Blessing|Crystal|Titan)\s*/gi, '($1)?').replace(/Noble\s*/g, '(Noble|Hisuian)?\\s*').replace('Toxtricity', 'Toxtri(city|town|island)?city').replace('Cosmog', 'Cosmog|Nebby');
 const evolutionsNormalized = (evolution) => evolution.replace(/\W|_/g, '.?').replace(/(Level)\s*/gi, '($1)?');
 const pokemonNameAnswer = (name) => new RegExp(`^\\W*${pokemonNameNormalized(name)}\\b`, 'i');
 const translatePokemonName = (name) => {
