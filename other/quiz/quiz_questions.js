6f48be9cb8080938cec4883548f2d06c22628ec8
Danial <admin@redsparr0w.com>
2026-07-23 21:14:54 +1200

    Add Milk as an answer for Muk
    closes #92
    
    Co-Authored-By: RedzepR <143606926+RedzepR@users.noreply.github.com>

--- other/quiz/quiz_questions.js
+++ other/quiz/quiz_questions.js
@@ -67,6 +67,7 @@ const normalizations = [
   [/Marill/gu, 'Marill|Pikablu'],
   [/Omanyte/gu, 'Omanyte|Lord Helix'],
   [/Nihilego/gu, 'Nihilego|Gustavo'],
+  [/Muk/gu, 'Muk|Milk'],
   [/\.\?\.\?\.\?/gu, '.*'],
 ];
 
