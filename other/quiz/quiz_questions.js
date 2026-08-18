5e27eee66435045f25fbd25aa0ff6f84fe640ce5
Smokefrost <smoke.frosttt@gmail.com>
2026-08-18 21:41:36 +0100

    unweight

--- other/quiz/quiz_questions.js
+++ other/quiz/quiz_questions.js
@@ -1279,7 +1279,7 @@ const quizTypes = [
   new WeightedOption(whosThatPokemon, 150),
   new WeightedOption(pokemonType, 85),
   new WeightedOption(howDoesThisPokemonEvolve, 80),
-  new WeightedOption(pokemonEvolvesBy, 50),
+  new WeightedOption(pokemonEvolvesBy, 25),
   new WeightedOption(whosThePokemonEvolution, 80),
   new WeightedOption(whosThePokemonPrevolution, 80),
   new WeightedOption(pokemonRegion, 45),
